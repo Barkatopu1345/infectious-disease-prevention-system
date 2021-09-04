7abeb8e07b476e5c43d4c27e8c2931e1ba85d0b9
Barkatopu <barkatopu1345@gmail.com>
2021-09-04 11:49:32 +0600

    led controlling file is found! everything is okk. just menu() need to be write and firmata file by arduino need to be uploaded before final run

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from face_recognization import *
 from mask import *
+from arduino_main import *
 
+
+print("Enter 1 for attendence system")
+
+print("Enter 2 for face mask system")
+
+decission = input("Enter your decission: ")
 
 main_face()
 maskMain()
+handCommand()
+voiceCommand()
